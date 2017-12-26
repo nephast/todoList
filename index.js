244697512b192d88dca627d2a1d4177d8ebe82aa
Stéphan Gaud <stephgoeseast@gmail.com>
2017-12-26 17:20:34 +0000

    SAP retrieve data from JSON API and display it in list. Link to POST route done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
     
     app.get('/', function(req, res) {
         res.sendFile('index.html');
--- public/app.js
+++ public/app.js
@@ -1,1 +1,32 @@
-alert('HELLO');
+$(document).ready(function() {
+$.getJSON('/api/todos')
+.then(addTodos)
+.catch(e => console.log(e))
+
+$('#todoInput').keypress(function(event) {
+    if(event.which == 13) {
+        createTodo();
+    }
+})
+});
+
+function addTodos(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
+function addTodo(todo) {
+    let newTodo = $('<li class="task">' + todo.name + '</li>')
+    if(todo.completed){
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    let userInput = $('#todoInput').val();
+    $.post('api/todos', {name: userInput})
+    .then(newTodo => {
+        $('#todoInput').val('')
+        addTodo(newTodo)})
+    .catch(e => console.log(e))
+}
