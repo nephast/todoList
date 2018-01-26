183ceb52f18c231f60ef31afd97d5bea738e0a0c
Stéphan Gaud <stephgoeseast@gmail.com>
2018-01-26 16:08:53 +0000

    wip

--- public/app.js
+++ public/app.js
@@ -9,8 +9,13 @@ $('#todoInput').keypress(function(event) {
     }
 })
 
-$('.list').on('click', 'span', function() {
-    removeTodo($(this).parent())
+$('.list').on('click', 'span', function(e) {
+    e.stopPropagation();
+    removeTodo($(this).parent());
+})
+
+$('.list').on('click', 'li', function() {
+    updateTodo($(this));
 })
 });
 
@@ -21,6 +26,7 @@ function addTodos(todos) {
 function addTodo(todo) {
     let newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if(todo.completed){
         newTodo.addClass('done');
     }
@@ -45,4 +51,20 @@ function removeTodo(todo) {
     })
     .then(() => todo.remove())
     .catch(e => console.log(e))
+}
+
+function updateTodo(todo) {
+    let clickedId = todo.data('id');
+    let updateUrl = 'api/todos/' + clickedId;
+    let isDone = !todo.data('completed');
+    let updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+    })
+    .then(updatedTodo => {
+        todo.toggleClass('done');
+        todo.data('completed', isDone);
+    })
 }
